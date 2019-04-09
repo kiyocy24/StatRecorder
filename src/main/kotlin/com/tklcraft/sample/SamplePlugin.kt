60d10865a2404fa5325157635313e0802cb85105
kiyocy24 <kiyocy24@gmail.com>
2019-04-09 21:37:40 +0900

    initial commit

---
+++ src/main/kotlin/com/tklcraft/sample/SamplePlugin.kt
@@ -0,0 +1,15 @@
+package com.tklcraft.sample
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SamplePlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
