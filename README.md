925c132e4742ca34f2b23ccc3a2432bf95d655ef
kiyocy24 <kiyocy24@gmail.com>
2020-08-06 00:25:22 +0900

    Update gradle and other components

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# Sample plugin
-
-Spigotプラグインのサンプルです。
-Kotlinで書きました。
-
-チュートリアル解説はこちらをどうぞ  
-[【Qiita】Spigotプラグイン開発 チュートリアル](https://qiita.com/kanakiyo314/items/a9190acb13266ad198e1)
+# Statistics Notifier Plugin
--- src/main/kotlin/com/tklcraft/sample/StatisticsNotifierPlugin.kt
+++ src/main/kotlin/com/tklcraft/sample/StatisticsNotifierPlugin.kt
@@ -2,7 +2,7 @@ package com.tklcraft.sample
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class SamplePlugin : JavaPlugin() {
+class StatisticsNotifier : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
